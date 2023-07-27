4b3191c9485de25040dcb858515cc39a371c9a75
AlexisLeray <alxs.leray@gmail.com>
2023-07-27 17:47:01 +0200

    suppresion du cube test page technique

--- src/components/Technical.jsx
+++ src/components/Technical.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 const Technical =() => {
+
+    //  const [cube, setCube] = useState({ x: 0, y: 0 });
+    // const moveObject = () => {
+    //        setCube({ x: cube.x + 130, y: cube.y + 10 });
+    // }
+
     return(
-        <h2>
-            Technical
-        </h2>
+        <>
+            <div className="cubeContainer">
+                <h2>
+                    Technical
+                </h2>
+                {/* <div className="cube" onClick={moveObject} style={{ top: cube.y + 'px', left: cube.x + 'px' }}> */}
+                {/* </div> */}
+            </div>    
+        </>
     )
 }
 
