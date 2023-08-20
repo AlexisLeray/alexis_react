a2cfec7bb9ad255627f03faea81ac74331ef3763
AlexisLeray <alxs.leray@gmail.com>
2023-08-20 17:44:07 +0200

    changement du carousel3 pour react-responsive-carousel

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,67 +1,95 @@
-import React,{useContext, Fragment, useEffect, useState} from "react";
-import {UserContext} from './UserContext'
-import Nav from './Nav'
+import React, { useContext, Fragment, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
+import Nav from "./Nav";
 
 const Presentation = () => {
-    const {userInfo} = useContext(UserContext);
-   
-    return(
+  const { userInfo } = useContext(UserContext);
+
+  return (
+    <>
+      {userInfo ? (
         <>
-            
-            {userInfo ?
-                <>
-                    <header>
-                        <Nav />
-                        <p>Bonjour {userInfo.name}, déjà merci pour la visite, bienvenue chez moi si je puis dire.</p>
-                        <p>L'utilitée de demander le prénom du visiteur n'est éssentielle, qu'on soit d'accord, néanmoins il me permettais de m'entrainer avec le localstorage, ce qui est toujours intéressant.</p>
-                        {/* <p> 
+          <header>
+            <Nav />
+            <p>
+              Bonjour {userInfo.name}, déjà merci pour la visite, bienvenue chez
+              moi si je puis dire.
+            </p>
+            <p>
+              L'utilitée de demander le prénom du visiteur n'est éssentielle,
+              qu'on soit d'accord, néanmoins il me permettais de m'entrainer
+              avec le localstorage, ce qui est toujours intéressant.
+            </p>
+            {/* <p> 
                             Comment {userInfo.userLanguage ? "vas-tu ?" : "allez-vous ?" }
                         </p> */}
-                        <h2>Comment sommes nous arrivés là ?</h2>
-                    </header>
-                    <section>
-                        <img src={process.env.PUBLIC_URL + '/profil.jpg'} alt="Alexis" className="photo"/>  
-                        <header>
-                            <p>
-                                Alexis Leray, 32 ans à l'heure actuelle, sauf si cette visite à lui après le 24 Octobre 2023 où là j'aurais 33 ans.
-                            </p>
-                        </header>
-                        <main>
-                            <p>
-                                Issu d'une formation initiale en boulangerie, j'ai dû arrêter mon métier de base, 
-                                car passionné de théâtre, j'ai un peu tiré sur la corde et le corps à dit stop.
-                            </p>
-                            <p>
-                                Première session de chômage et je commence déjà à m'intéresser au métiers de l'informatique, 
-                                mais mal renseingé et timing un peu cours pour se remettre de la santé et 
-                                trouver ma voie je la laisse en suspend pendant 4 ans.
-                            </p>
-                            <p>
-                                Tant d'années passée dans une métier qui ne me convenait pas, 
-                                mais dans lequel j'ai pû apprendre l'importance et l'art subtil du travail en équipe il était temps pour moi 
-                                de passer dans une autre entreprise pour voir de nouvelles choses et, vue que j'y suis resté que quelques mois, 
-                                visiblement ça ne l'a pas fait.
-                            </p>
-                            <p>
-                                Nouvelle session de chômage, nouvelles opportunités ! Retour aux sources et à ce qui m'intéressait principalement, l'informatique ! Beaucoup de choix possibles, alors pourquoi le développement web ?  
-                            </p>
-                            <p>
-                                Simplement parce que j'ai un très bon ami dans le milieu, j'ai pû et j'en observe encore les tréfonds, sans forcément tout comprendre, mais en comprenant que ne pas tout comprendre fait aussi partie du métier.
-                            </p>
-                            <p>
-                                Quand une opportunité se présente il faut savoir la saisir, un soir un mail automatique me propose une formation avec la <a href="https://3wa.fr/" target="_blank" rel="noreferrer">3W Academy</a>, l'occasion pour moi de commencer réellement mon apprentissage. 
-                            </p>
-                            <p>
-                                Le diplôme obtenu avec la mention très bien , {userInfo.userLanguage ? "s'il te plaît" : "s'il vous plaît" }, il faut poursuivre sur la lancée avec un autre diplôme et d'autres connaissances ! 
-                            </p>
-                        </main>
-                    </section>
-                </>    
-                : 
-                <p>Bonjour çamarchepas</p>
-            }
+            <h2>Comment sommes nous arrivés là ?</h2>
+          </header>
+          <section>
+            <img
+              src={process.env.PUBLIC_URL + "/profil.jpg"}
+              alt="Alexis"
+              className="photo"
+            />
+            <header>
+              <p>
+                Alexis Leray, 32 ans à l'heure actuelle, sauf si cette visite à
+                lui après le 24 Octobre 2023 où là j'aurais 33 ans.
+              </p>
+            </header>
+            <main>
+              <p>
+                Issu d'une formation initiale en boulangerie, j'ai dû arrêter
+                mon métier de base, car passionné de théâtre, j'ai un peu tiré
+                sur la corde et le corps à dit stop.
+              </p>
+              <p>
+                Première session de chômage et je commence déjà à m'intéresser
+                au métiers de l'informatique, mais mal renseingé et timing un
+                peu cours pour se remettre de la santé et trouver ma voie je la
+                laisse en suspend pendant 4 ans.
+              </p>
+              <p>
+                Tant d'années passée dans une métier qui ne me convenait pas,
+                mais dans lequel j'ai pû apprendre l'importance et l'art subtil
+                du travail en équipe il était temps pour moi de passer dans une
+                autre entreprise pour voir de nouvelles choses et, vue que j'y
+                suis resté que quelques mois, visiblement ça ne l'a pas fait.
+              </p>
+              <p>
+                Nouvelle session de chômage, nouvelles opportunités ! Retour aux
+                sources et à ce qui m'intéressait principalement, l'informatique
+                ! Beaucoup de choix possibles, alors pourquoi le développement
+                web ?
+              </p>
+              <p>
+                Simplement parce que j'ai un très bon ami dans le milieu, j'ai
+                pû et j'en observe encore les tréfonds, sans forcément tout
+                comprendre, mais en comprenant que ne pas tout comprendre fait
+                aussi partie du métier.
+              </p>
+              <p>
+                Quand une opportunité se présente il faut savoir la saisir, un
+                soir un mail automatique me propose une formation avec la{" "}
+                <a href="https://3wa.fr/" target="_blank" rel="noreferrer">
+                  3W Academy
+                </a>
+                , l'occasion pour moi de commencer réellement mon apprentissage.
+              </p>
+              <p>
+                Le diplôme obtenu avec la mention très bien ,{" "}
+                {userInfo.userLanguage ? "s'il te plaît" : "s'il vous plaît"},
+                il faut poursuivre sur la lancée avec un autre diplôme et
+                d'autres connaissances !
+              </p>
+            </main>
+          </section>
         </>
-    )
-}
+      ) : (
+        <p>Bonjour çamarchepas</p>
+      )}
+    </>
+  );
+};
 
-export default Presentation
+export default Presentation;
--- src/components/Technical.jsx
+++ src/components/Technical.jsx
@@ -1,90 +1,84 @@
-import React, { useState,useEffect } from 'react'
-import { Carousel } from 'react-carousel3';
+import React, { useState, useEffect } from "react";
+import Nav from "./Nav";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
+const Technical = () => {
+  const data = [
+    {
+      id: 1,
+      name: "html",
+      img: "/images/html.png",
+    },
+    {
+      id: 2,
+      name: "css",
+      img: "/images/css.png",
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      img: "/images/javascript.png",
+    },
+    {
+      id: 4,
+      name: "React",
+      img: "/images/react.png",
+    },
+    {
+      id: 5,
+      name: "NodeJs",
+      img: "/images/node.png",
+    },
+    {
+      id: 6,
+      name: "MySQL",
+      img: "/images/mysql.png",
+    },
+    {
+      id: 7,
+      name: "Git",
+      img: "/images/git.png",
+    },
+    {
+      id: 8,
+      name: "Github",
+      img: "/images/GitHub.png",
+    },
+  ];
 
-const Technical =() => {
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      {/* <button type="submit" onClick={test}>test</button> */}
+      <div className="cubeContainer">
+        <h2>Technical</h2>
+        <div className="carousel">
+          <Carousel
+            // height={460}
+            width="25%"
+            yOrigin={42}
+            yRadius={48}
+            autoPlay={true}
+            stopOnHover={true}
+            swipeable={true}
+            showArrows={true}
+          >
+            {data.map((e, i) => {
+              return (
+                <div key={i} className="carousel__content">
+                  <img src={e.img} className="carousel__img" />
+                  <h3>{e.name}</h3>
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    //  const [cube, setCube] = useState({ x: 0, y: 0 });
-    // const moveObject = () => {
-    //        setCube({ x: cube.x + 130, y: cube.y + 10 });
-    // }
-    const data = [
-        {
-            id: 1,
-            name: "html",
-            img: "/images/html.png",
-
-        },
-        {
-            id:2,
-            name: "css",
-            img: "/images/css.png"
-        },
-        {
-            id: 3,
-            name: "JavaScript",
-            img: "/images/javascript.png",
-
-        },
-        {
-            id: 4,
-            name: "React",
-            img: "/images/react.png",
-        },
-        {
-            id: 5,
-            name: "NodeJs",
-            img: "/images/node.png",
-
-        },
-        {
-            id: 6,
-            name: "MySQL",
-            img: "/images/mysql.png",
-
-        },
-        {
-            id: 7,
-            name: "Git",
-            img: "/images/git.png",
-
-        },
-        {
-            id: 8,
-            name: "Github",
-            img: "/images/GitHub.png",
-
-        },
-    ];
-
-
-    const test = (e) => {
-         e.preventDefault()
-        
-         console.log(data)
-     }
-    return(
-        <>
-            <button type="submit" onClick={test}>test</button>
-            <div className="cubeContainer">
-                <h2>
-                    Technical
-                </h2>
-                <div className="carousel">
-                    <Carousel height={460} width={980} yOrigin={42} yRadius={48} autoPlay={true}>
-                        {data.map((e,i)=> {
-                            return(
-                                <div key={i}>
-                                    <img src={e.img} className="carousel__img"/>
-                                    <h3>{e.name}</h3>
-                                </div>
-                            )
-                        })}
-                    </Carousel>
-               </div> 
-            </div>    
-        </>
-    )
-}
-
-export default Technical
+export default Technical;
